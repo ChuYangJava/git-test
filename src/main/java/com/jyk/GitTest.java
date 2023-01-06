1c54acc35a2d3865fbbf95e20da75eef4e391576
chuyang <1069477561@qq.com>
2023-01-06 15:48:58 +0800

    master second commit

--- src/main/java/com/jyk/GitTest.java
+++ src/main/java/com/jyk/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello!初阳");
         System.out.println("hello!Linux");
         System.out.println("hot-fix分支修改");
+        System.out.println("master分支修改");
     }
 }
