e8ed57ce6bea982fbcc2c1a07cf3082849f734c0
chuyang <1069477561@qq.com>
2023-01-06 15:34:45 +0800

    first commit

---
+++ src/main/java/com/jyk/GitTest.java
@@ -0,0 +1,11 @@
+package com.jyk;
+
+/**
+ * @author 江雨坤
+ * @create 2023-01-06-15:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello!Git");
+    }
+}
